f9a47a9b0749f11ba62d9148a0af6df6ab7a2e04
Akash <34833061+machraakash@users.noreply.github.com>
2024-06-20 16:21:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 var randomNumber1 = Math.random();
-randomNumber1=randomNumber1*6;
-randomNumber1=Math.floor(randomNumber1)+1;
+randomNumber1 = randomNumber1 * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1;
 
-document.getElementsByClassName("img1")[0].src = "./images/dice"+randomNumber1+".png";
+var randomNumber2 = Math.random();
+randomNumber2 = randomNumber2 * 6;
+randomNumber2 = Math.floor(randomNumber2) + 1;
+
+document.getElementsByClassName("img1")[0].src =
+    "./images/dice" + randomNumber1 + ".png";
+document.getElementsByClassName("img2")[0].src =
+    "./images/dice" + randomNumber2 + ".png";
+
+if (randomNumber1 > randomNumber2) {
+    document.getElementsByTagName("h1")[0].innerHTML="Player 1 Wins!";
+} 
+else if (randomNumber1 ==  randomNumber2){
+    document.getElementsByTagName("h1")[0].innerHTML="Draw!";
+}
+
+else {
+    document.getElementsByTagName("h1")[0].innerHTML="Player 2 Wins!";
+}
