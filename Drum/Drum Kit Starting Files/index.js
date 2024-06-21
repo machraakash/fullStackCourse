9b2bf45ec2318d22cd0be9762180c22deccecb6f
Akash <34833061+machraakash@users.noreply.github.com>
2024-06-21 16:03:43 +0530

    Delete

--- Drum/Drum Kit Starting Files/index.js
+++
@@ -1,6 +0,0 @@
-
-for(i=0 ; i < document.getElementsByClassName("drum").length  ; i++){
-document.getElementsByClassName("drum")[i].addEventListener("click", function(){
-    alert("I got clicked!");
-});
-}
