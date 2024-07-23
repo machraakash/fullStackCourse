cf108d776d18416196429a85e22e0163d9463456
Akash <34833061+machraakash@users.noreply.github.com>
2024-07-23 13:52:45 +0530

    Add files via upload

---
+++ Simon/game.js
@@ -0,0 +1,8 @@
+var buttonColors = ["red","blue","green","yellow"];
+
+function nextSequence(){
+    var randomNumber = Math.ceil((Math.random())*4);
+    return randomNumber;
+};
+
+// var randomChosenColor = ;
