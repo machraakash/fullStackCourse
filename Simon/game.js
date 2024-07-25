70f31e558b3dd7b794a1ef3edf1073b38db95090
Akash <34833061+machraakash@users.noreply.github.com>
2024-07-25 11:27:36 +0530

    Add files via upload

--- Simon/game.js
+++ Simon/game.js
@@ -1,8 +1,32 @@
 var buttonColors = ["red","blue","green","yellow"];
 
 function nextSequence(){
-    var randomNumber = Math.ceil((Math.random())*4);
+    var randomNumber = Math.floor((Math.random())*4);
     return randomNumber;
 };
 
-// var randomChosenColor = ;
+var randomChosenColor = {
+    chosenColor : function(){
+        switch(nextSequence()){
+            case 0:
+                var currentlyChosenColor = buttonColors[0];
+            break;
+
+            case 1:
+                var currentlyChosenColor = buttonColors[1];
+            break;
+
+            case 2:
+                var currentlyChosenColor = buttonColors[2];
+            break;
+
+            case 3:
+                var currentlyChosenColor = buttonColors[3];
+            break;
+
+            default:
+                alert("Error in the switch statement." + nextSequence());
+        };
+        return currentlyChosenColor;
+    }
+};
