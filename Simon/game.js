b814b62133bf33a7dd0945faeb9989ef04d75e92
Akash <34833061+machraakash@users.noreply.github.com>
2024-07-26 13:20:21 +0530

    Add files via upload

--- Simon/game.js
+++ Simon/game.js
@@ -1,12 +1,32 @@
-var buttonColors = ["red","blue","green","yellow"];
+var userClickedPattern = [];
+
+var buttonColors = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 
-function nextSequence(){
-    var randomNumber = Math.floor((Math.random())*4);
-    return randomNumber;
-};
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  return randomNumber;
+}
 
 var randomChosenColor = buttonColors[nextSequence()];
 
 gamePattern.push(randomChosenColor);
+
+$("#" + randomChosenColor)
+  .animate({ opacity: 0.2 }, 100)
+  .animate({ opacity: 1.0 }, 100);
+
+// var buttonAudio = new Audio("sounds/blue.mp3");
+// buttonAudio.play();
+
+$(".btn").on("click", function (event) {
+  var userChosenColour = '"' + event.target.id + '"';
+//   console.log(userChosenColour);
+userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+});
+
+
+
+console.log(userClickedPattern);
