b9483dbbec5d6680245816c6ee932b8fdf6618b1
Akash <34833061+machraakash@users.noreply.github.com>
2024-07-27 07:44:46 +0530

    Add files via upload

--- Simon/game.js
+++ Simon/game.js
@@ -5,8 +5,8 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 
 function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 4);
-  return randomNumber;
+    var randomNumber = Math.floor(Math.random() * 4);
+    return randomNumber;
 }
 
 var randomChosenColor = buttonColors[nextSequence()];
@@ -14,19 +14,20 @@ var randomChosenColor = buttonColors[nextSequence()];
 gamePattern.push(randomChosenColor);
 
 $("#" + randomChosenColor)
-  .animate({ opacity: 0.2 }, 100)
-  .animate({ opacity: 1.0 }, 100);
-
-// var buttonAudio = new Audio("sounds/blue.mp3");
-// buttonAudio.play();
-
+    .animate({ opacity: 0.2 }, 100)
+    .animate({ opacity: 1.0 }, 100);
+    
 $(".btn").on("click", function (event) {
-  var userChosenColour = '"' + event.target.id + '"';
-//   console.log(userChosenColour);
-userClickedPattern.push(userChosenColour);
-  console.log(userClickedPattern);
+    var userChosenColour = event.target.id;
+    userClickedPattern.push(userChosenColour);
+
+    console.log(userClickedPattern);
+
+    var buttonAudio = new Audio("sounds/" + userChosenColour + ".mp3");
+    buttonAudio.play();
+    console.log(buttonAudio);
 });
 
+function playSound(name){
 
-
-console.log(userClickedPattern);
+};
