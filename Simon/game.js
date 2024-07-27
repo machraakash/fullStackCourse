1368470a4e8c8006180cade32f6b6bc799e07b94
Akash <34833061+machraakash@users.noreply.github.com>
2024-07-27 10:40:36 +0530

    Add files via upload

--- Simon/game.js
+++ Simon/game.js
@@ -1,33 +1,48 @@
 var userClickedPattern = [];
-
 var buttonColors = ["red", "blue", "green", "yellow"];
-
 var gamePattern = [];
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
-    return randomNumber;
+
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+    console.log("Randomly Generated Color: " + gamePattern);
+
+    $("#" + randomChosenColor)
+        .animate({ opacity: 0.2 }, 100)
+        .animate({ opacity: 1.0 }, 100);
+
+    playSound(randomChosenColor);
+    
+
+    $(".btn").on("click", function (event) {
+        var userChosenColour = event.target.id;
+        userClickedPattern.push(userChosenColour);
+        console.log("Click Generated userClickedPattern Array: [" + userClickedPattern + "]");
+        playSound(userChosenColour);
+        animatePress(userChosenColour)
+    });
 }
 
-var randomChosenColor = buttonColors[nextSequence()];
+//Might be an issue here. The value of randomChosenColor doesn't change after reload. So multiple values are not being stored in gamePattern.
+// Maybe this code should be a part of nextSequece(). 
+// Just saw the comment on udemy. Indeed it should be inside the function. Added.
+nextSequence();
 
-gamePattern.push(randomChosenColor);
 
-$("#" + randomChosenColor)
-    .animate({ opacity: 0.2 }, 100)
-    .animate({ opacity: 1.0 }, 100);
-    
-$(".btn").on("click", function (event) {
-    var userChosenColour = event.target.id;
-    userClickedPattern.push(userChosenColour);
-
-    console.log(userClickedPattern);
-
-    var buttonAudio = new Audio("sounds/" + userChosenColour + ".mp3");
+function playSound(name) {
+    var buttonAudio = new Audio("sounds/" + name + ".mp3");
     buttonAudio.play();
     console.log(buttonAudio);
-});
+};
 
-function playSound(name){
-
+function animatePress(currentColor) {
+    $(".btn").on("click", function (event) {
+        var userClickedButton = event.target.id;
+        $("#" + userClickedButton).addClass("pressed", 1000);
+        setTimeout(function () {
+            $("#" + userClickedButton).removeClass("pressed");
+        }, 100);
+    })
 };
