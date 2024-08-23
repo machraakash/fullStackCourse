446bdf10acbf7ca3e07d2e7de7d6227544e45dc8
Akash <34833061+machraakash@users.noreply.github.com>
2024-08-23 12:11:11 +0530

    Add files via upload

--- Node/2.3 NPM/index.js
+++ Node/2.3 NPM/index.js
@@ -1,4 +1,9 @@
-var sillyName = require('sillyname');
+// var sillyName = require('sillyname');
+import sillyName from 'sillyname';
+import { randomSuperhero } from 'superheroes';
 var myName = sillyName();
 
-console.log(`My name is ${myName}.`);
+// console.log(`My name is ${myName}.`);
+
+
+console.log(`My superhero name is ${randomSuperhero()}.`);
