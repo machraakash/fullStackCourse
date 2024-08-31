9773b8a91b7d49d9d4ec85dd9fa8235b596add04
Akash <34833061+machraakash@users.noreply.github.com>
2024-08-31 15:12:12 +0530

    Add files via upload

--- Node/index.js
+++ Node/index.js
@@ -1,48 +1,41 @@
-/* 
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
+// 1. Use the inquirer npm package to get user input.
 
-// import inquirer from 'inquirer';
-// inquirer
-// .prompt([{
-//     name: "Website: ",
-// }
-//   ])
-//   .then((answers) => {
+import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from 'fs';
 
-//     console.log(answers);
-//   });
+inquirer
+    .prompt([
+        {
+            name: "Akash",
+            message: "Enter the website that you wish to make a qr code image out of",
+        }
+    ])
+    .then((answers) => {
+        var url = answers.Akash;
+        console.log(url);
 
-// const fs = require("fs");
-// import qr from 'qr-image';
-var titties = require('qr-image');
+        // 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 
-// var qrText = qr();
+        var qr_svg = qr.image(url);
+        qr_svg.pipe(fs.createWriteStream('url.png'));
 
-// qr.image("schomoyo")
-const qr = titties.create('https://www.yokohoma.com', { type: 'png', size: 10, margin: 1 });
-console.log(qr.toString('base64'));
-// var userAddress = prompt("Enter the website address, for QR code generation.");
+        // 3. Create a txt file to save the user input using the native fs node module.
 
-// var qr_svg = qr.image(userAddress, { type: 'png' });
-// qr_svg.pipe(require('fs').createWriteStream(userAddress + '.png'));
+        fs.writeFile('URL History.txt', url, (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+          }); 
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else went wrong
+        }
+    });
 
-// fs.writeFile("message.txt", userAddress, (err) => {
-//         if(err) throw err;
-//         console.log("USAA MISAAAA!");
-//     });
-// .prompt([
-//     /* Pass your questions in here */
-//   ])
-//   .then((answers) => {
-//     // Use user feedback for... whatever!!
-//   })
-//   .catch((error) => {
-//     if (error.isTtyError) {
-//       // Prompt couldn't be rendered in the current environment
-//     } else {
-//       // Something else went wrong
-//     }
-//   });
+
+
+
+
