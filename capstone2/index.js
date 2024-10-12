faaa1523957bd8c7dcf1fbd2cee918d423c3af27
Akash <34833061+machraakash@users.noreply.github.com>
2024-10-12 11:02:34 +0530

    Add files via upload

--- capstone2/index.js
+++ capstone2/index.js
@@ -6,9 +6,25 @@ const port = 3000;
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
-  });
+  res.render("index.ejs");
+});
+
+app.get("/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.get("/edit", (req, res) => {
+  res.render("edit.ejs");
+});
+
+app.get("/read", (req, res) => {
+  res.render("read.ejs");
+});
+
+// app.post("/submit", (req, res) => {
+//   res.render("index.ejs", {wordLength: req.body["fName"]});
+// });
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-  });
+  console.log(`Listening on port ${port}`);
+});
