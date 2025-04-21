6ab0a9668a993c9fbb57d10bde4a6d1310537095
machraakash <machra.akash@gmail.com>
2025-04-21 18:43:30 +0530

    connecting to postgres for visited_countries challenge

--- SQL/Postgres/8.3 Travel Tracker/index.js
+++ SQL/Postgres/8.3 Travel Tracker/index.js
@@ -1,8 +1,27 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "akash",
+  password: "postgres23579",
+  port: 5432,
+});
 const app = express();
 const port = 3000;
+
+db.connect();
+
+db.query("SELECT * FROM visited_countries", (err, res) => {
+  if(err){
+    console.error("ERROR EXECUTING QUERY", err.stack);
+  }else{
+    console.log("Countries Visited:", res.rows);
+  }
+  db.end();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
