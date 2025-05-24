5be71825db62ef71f816d079f3751ac0e5ffbdc5
Akash <34833061+machraakash@users.noreply.github.com>
2025-05-24 20:41:04 +0530

    Add files via upload

--- SQL/Postgres/8.3 Travel Tracker/index.js
+++ SQL/Postgres/8.3 Travel Tracker/index.js
@@ -5,24 +5,52 @@ import pg from "pg";
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "Akash",
-  password: "Postgres",
+  database: "akash",
+  password: "postgres23579",
   port: 5432,
 });
 const app = express();
 const port = 3000;
 
 db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
+async function checkVisisted() {
   const result = await db.query("SELECT country_code FROM visited_countries");
+
   let countries = [];
-  result.rows.forEach((country) =>{
+  result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
-  res.render("index.ejs", {countries: countries, total: countries.length});
+  return countries;
+}
+
+// GET home page
+app.get("/", async (req, res) => {
+  const countries = await checkVisisted();
+  res.render("index.ejs", { countries: countries, total: countries.length });
+});
+
+//INSERT new country
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+
+  const result = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = $1",
+    [input]
+  );
+
+  if (result.rows.length !== 0) {
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+      countryCode,
+    ]);
+    res.redirect("/");
+  }
   db.end();
 });
 
